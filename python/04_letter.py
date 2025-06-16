4ee90152e2d678b014dd243ad74e81aa192dbf6b
londonsolcodes <londonsol.codes@gmail.com>
2025-06-16 16:35:46 -0400

    Create 04_letter.py

---
+++ python/04_letter.py
@@ -0,0 +1,5 @@
+# Locked In
+print('16th June, 2025')
+print('Im feeling accomplished, and optimistic about my future in tech')
+print('Hello London, I hope youre doing well. Hopefully we didnt let the doubts of others determine our fate. I know youre a hard worker, and youre capable of amazing things. If theyre hating now, imagine them when youre a Software Enginner at a big tech company. I would be mad too lol')
+print('smiley face emoji')
