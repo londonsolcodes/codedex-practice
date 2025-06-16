5145ea2ea1b85a38f292e6cc876ad346551d8c14
londonsolcodes <londonsol.codes@gmail.com>
2025-06-16 16:21:41 -0400

    Create 03.intials.py

---
+++ python/03.intials.py
@@ -0,0 +1,6 @@
+# My name is London Solnuri Love, and I am a future Software Enginner
+print('L       SSS    L')
+print('L      S       L')
+print('L       SSS    L')
+print('L           S  L')
+print('LLLL    SSSS   LLLL')
