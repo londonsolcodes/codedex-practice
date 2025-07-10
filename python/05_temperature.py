d46ec4a2207c5b5aa25e6ead1bfc7536744c4617
londonsolcodes <londonsol.codes@gmail.com>
2025-07-10 19:19:13 -0400

    Create 05_temperature.py

---
+++ python/05_temperature.py
@@ -0,0 +1,6 @@
+#Create a temperature.py program that converts a number from Fahrenheit (°F) to Celsius (°C). Google the current temperature of Brooklyn, NY (where Codédex is based) in °F. Use the following formula and write it out in Python:
+# It is 78 degreeds Fahrenheit in Brooklyn, as of right now.
+
+temp_f = 78
+temp_c = (temp_f - 32) / 1.8
+print(temp_c)
